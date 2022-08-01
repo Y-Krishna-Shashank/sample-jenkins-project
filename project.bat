53a5bd8cc73cec176fe75227a6265e791f172b1e
Y-Krishna-Shashank <94391991+Y-Krishna-Shashank@users.noreply.github.com>
2022-08-01 12:58:56 +0530

    Update project.bat

--- project.bat
+++ project.bat
@@ -1,1 +1,1 @@
-echo "This is jenkins sample project for webhook and poll scm, testing webhook, trial: 5"
+echo "This is jenkins sample project for webhook and poll scm, testing webhook, trial: 6 12:58 PM"
