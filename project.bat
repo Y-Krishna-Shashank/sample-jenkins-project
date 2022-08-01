baea686c33a68ec6ba3f780134945eda408dc185
Y-Krishna-Shashank <94391991+Y-Krishna-Shashank@users.noreply.github.com>
2022-08-01 13:31:42 +0530

    Update project.bat

--- project.bat
+++ project.bat
@@ -1,1 +1,1 @@
-echo "This is jenkins sample project for webhook and poll scm, testing webhook, trial 7 with different port(8081)"
+echo "This is jenkins sample project for webhook and poll scm, testing webhook, trial 8 after changing the port back to 8080"
